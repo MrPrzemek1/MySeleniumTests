b89522aff12bf93ec2e2d7c29a2c8339459bad6c
Przemysław Mroczek <przemyslaw.mroczek@netrix.com.pl>
2018-03-21 08:05:20 +0100

    Dodanie klas.

---
+++ TestyRawa/Steps/StepOneShapes.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System.Collections.Generic;
+using static TestyRawa.DriverHelper.Browser;
+using System.Linq;
+namespace TestyRawa
+{
+    public class StepOneShapes
+    {
+        private ICollection<IWebElement> RoomParamsList => Driver.FindElements(By.XPath("//type[@number]"));
+        private IList<IWebElement> ShapesRoomList => Driver.FindElements(By.XPath("//li[@shape-id]"));
+        private IWebElement Room3DView => Driver.FindElement(By.XPath("//div[@class='room3dView']"));
+        private IWebElement costam => Driver.FindElement(By.ClassName("asdasd"));
+        public bool ViewRoomList()
+        {
+            WaitUntilElementIsDisplayed((By.XPath("//div[@class='room3dView']")), 5);
+            IList<bool> ActiveClass = new List<bool>();
+            foreach (var item in ShapesRoomList)
+            {
+                item.Click();
+                string className = item.GetAttribute("class");
+
+                if (item.GetAttribute("Class").Equals("Active"))
+                {
+                    ActiveClass.Add(true);
+                }
+            }
+            if (ActiveClass.All(b => true))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ TestyRawa/Tests/StepOneTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static TestyRawa.DriverHelper;
+
+namespace TestyRawa
+{
+    [TestFixture]
+    class StepOneTests : StepOneShapes
+    {
+        [OneTimeSetUp]
+        public void TestInizialize()
+        {
+            Browser.Initialize();
+            Browser.WaitUntilElementIsDisplayed(By.XPath("//button[@class='btn btn-primary btn-lg btn-start']"), 5);
+            Browser.ClickOnElement(By.XPath("//button[@class='btn btn-primary btn-lg btn-start']"));
+        }
+        [OneTimeTearDown]
+        public void EndTest()
+        {
+            Browser.Quit();
+        }
+        [Test,Description("Przeglądanie kształtów pomieszczeń"),Order(1)]
+        public void Test()
+        {
+            ViewRoomList();
+        }
+    }
+}
