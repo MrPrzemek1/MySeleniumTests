c218ce4cf46aee658949ab389101f687841d67e9
Przemysław Mroczek <przemyslaw.mroczek@netrix.com.pl>
2018-03-27 15:32:52 +0200

    Dodanie modelu i servisu dla strony startowej

---
+++ RawaTests/Tests/HomePageTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using RawaTests.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static TestyRawa.DriverHelper;
+
+namespace RawaTests.Tests
+{
+    [TestFixture]
+    class HomePageTests
+    {
+        HomePageServices homePageSrv;
+        HomePageTests()
+        {
+            homePageSrv = new HomePageModel
+        }
+        
+        [SetUp]
+        public void TestInizialize()
+        {
+            Browser.Initialize();
+        }
+        [TearDown]
+        public void EndTest()
+        {
+            Browser.Quit();
+        }
+        [Test]
+        public void ButtonStartIsDisplayed()
+        {
+
+        }
+    }
+}
