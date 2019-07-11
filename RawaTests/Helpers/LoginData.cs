a6379350acf6f2eb7a6fdca29d42926ae833c295
MrPrzemek1 <32515669+MrPrzemek1@users.noreply.github.com>
2019-07-11 11:07:25 +0200

    Delete LoginData.cs

--- RawaTests/Helpers/LoginData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RawaTests.Helpers
-{
-    public static class LoginData
-    {
-        public const string CompanyName = "rmg";
-        public const string Login = "Radosław Gierach";
-        public const string Password = "MADZIAZZ";
-    }
-}
